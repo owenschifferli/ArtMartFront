2663efa2a9d66587461f97a012542dc0ca6a1704
Schifferli <oschiffe@rett-imac-20.ur.rochester.edu>
2018-12-15 16:52:16 -0500

    menu-highlighter changes

--- js/menu-highlighter.js
+++ js/menu-highlighter.js
@@ -1,60 +1,33 @@
-//	*****************************
-//	Prep...
-//	*****************************
-// This script depends on jQuery; put this in your HTML file...
-// 	<script src="http://code.jquery.com/jquery.js"></script>
-// 
-// And then link to this JS file...
-// 	<script src="js/menu-highlighter.js"></script>
-//
-// This script assumes you created some CSS to style the ".is-current" class
-// Put something like this in your CSS...
-// 	.is-current {
-// 		background-color: black;
-// 		color: white;
-// 	}
+$(document).ready(function() {
 
-
-//	*****************************
-//	The way this works...
-//	*****************************
-
-//	This function will return the current file name
-function currentFile() {
-	// Step 1: grab the current "location" (URL) of this webpage and put it in a variable
-	currentURL = window.location.href;
-
-	// Step 2: find the location (index number) of the last slash (/) in the URL
-	fileNameIndex = currentURL.lastIndexOf("/") + 1;
-
-	// Step 3: extract the filename from the URL based on the whatever's to the right of
-	//	the last slash
-	currentFileName = currentURL.substr(fileNameIndex);
-
-	return currentFileName;
-}
-
-thisFile = currentFile();
-
-// Step 4: if there's nothing to the right of the last slash, then the current page is
-//	index.php, so manually set it so
-if ( thisFile == "" ) {
-	thisFile = "index.html"
-}
-
-// Step 5: check each anchor in the navigation element 
-//	(btw - did you code your menu using NAV?)...
-$("nav a").each( function() {
-
-	// Step 5a: if there's a match...
-	if (this.href.indexOf(thisFile) > 0) {
-
-		// Step 5a-a: then add the "is-current" class to the anchor
-		$(this).addClass("is-current");
+	// Get current page URL
+	var url = window.location.href;
+   
+	// remove # from URL
+	url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+   
+	// remove parameters from URL
+	url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+   
+	// select file name
+	url = url.substr(url.lastIndexOf("/") + 1);
+	
+	// If file name not avilable
+	if(url == ''){
+	url = 'index.html';
 	}
-});
-
-console.log(currentURL, "currenturl");
-console.log(fileNameIndex, "filenameindex");
-console.log(currentFileName, "currentfilename");
-console.log(thisFile, "thisfile");
+	
+	// Loop all menu items
+	$('.menu li').each(function(){
+   
+	 // select href
+	 var href = $(this).find('a').attr('href');
+	
+	 // Check filename
+	 if(url == href){
+   
+	  // Add active class
+	  $(this).addClass('active');
+	 }
+	});
+   });
